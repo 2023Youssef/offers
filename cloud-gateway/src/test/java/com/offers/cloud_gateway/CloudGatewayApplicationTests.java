f7728c44ebff9d981ebad19a97fa4fcb9c7d7e2e
youssef <yamari@insea.ac.ma>
2024-10-28 22:35:43 +0100

    Initial commit

---
+++ cloud-gateway/src/test/java/com/offers/cloud_gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offers.cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
